1f2ecfc081b55126a8d7223a3e4c91551f7d57b2
seppel1 <s.giesse@googglemail.com>
2017-11-27 18:11:14 +0100

    set log level as cmdLine argument

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,7 @@
 #include "QuartetScoreComputer.hpp"
 
 #include "tclap/CmdLine.h"
+#include <tclap/ValuesConstraint.h>
 
 #include <string>
 #include <limits>
@@ -33,10 +34,6 @@ void doStuff(std::string pathToEvaluationTrees, int m) {
 int main(int argc, char* argv[]) {
     Logging::log_to_stdout ();
 
-    //kNone,kError,kWarning,kInfo,kProgress,kDebug,kDebug4
-
-    LOG_SCOPE_LEVEL(utils::Logging::kDebug);
-
     LOG_BOLD << "Compute quartet score based Tree" << std::endl;
 
     std::string pathToEvaluationTrees;
@@ -45,13 +42,30 @@ int main(int argc, char* argv[]) {
     try {
         TCLAP::CmdLine cmd("Compute quartet score based Tree", ' ', "1.0");
         TCLAP::ValueArg<std::string> refArg("r", "ref", "Path to the reference tree", false, "../../data/ICTC-master/data/Empirical/Yeast/yeast_reference.tre", "string");
-        TCLAP::ValueArg<std::string> evalArg("e", "eval", "Path to the evaluation trees", false, "../../data/ICTC-master/data/Empirical/Yeast/yeast_partial_only.tre", "string");
         cmd.add(refArg);
+        TCLAP::ValueArg<std::string> evalArg("e", "eval", "Path to the evaluation trees", false, "../../data/ICTC-master/data/Empirical/Yeast/yeast_partial_only.tre", "string");
         cmd.add(evalArg);
+
+        std::vector<std::string> allowedLogLevels = { "None","Error","Warning","Info","Progress","Debug","Debug1","Debug2","Debug3","Debug4" };
+        TCLAP::ValuesConstraint<std::string> constraintLogLevels(allowedLogLevels);
+        TCLAP::ValueArg<std::string> logLevelArg("l", "loglevel", "Log Level", false, "Info" , &constraintLogLevels);
+        cmd.add(logLevelArg);
+
         cmd.parse(argc, argv);
 
         pathToReferenceTree = refArg.getValue();
         pathToEvaluationTrees = evalArg.getValue();
+
+        if (logLevelArg.getValue() == "None") Logging::max_level(utils::Logging::kNone);
+        if (logLevelArg.getValue() == "Error") Logging::max_level(utils::Logging::kError);
+        if (logLevelArg.getValue() == "Warning") Logging::max_level(utils::Logging::kWarning);
+        if (logLevelArg.getValue() == "Info") Logging::max_level(utils::Logging::kInfo);
+        if (logLevelArg.getValue() == "Progress") Logging::max_level(utils::Logging::kProgress);
+        if (logLevelArg.getValue() == "Debug") Logging::max_level(utils::Logging::kDebug);
+        if (logLevelArg.getValue() == "Debug1") Logging::max_level(utils::Logging::kDebug1);
+        if (logLevelArg.getValue() == "Debug2") Logging::max_level(utils::Logging::kDebug2);
+        if (logLevelArg.getValue() == "Debug3") Logging::max_level(utils::Logging::kDebug3);
+        if (logLevelArg.getValue() == "Debug4") Logging::max_level(utils::Logging::kDebug4);
     } catch (TCLAP::ArgException &e) {
         std::cerr << "ERROR: " << e.error() << " for arg " << e.argId() << std::endl;
         return 1;
