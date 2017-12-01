703672398ca8d0153cc09364d40814d2b2cc16dc
seppel1 <s.giesse@googglemail.com>
2017-12-01 17:27:02 +0100

    in evaluation: start from same sart tree

--- eval/evaluate.py
+++ eval/evaluate.py
@@ -23,33 +23,44 @@ data = [('../../data/ICTC-master/data/Empirical/Avian/avian_all.tre',
 data = data[1:3]
 
 
-args1 = [TREESEARCH_EXCTBL, '-s', 'stepwiseaddition', '-a', 'nni', '-l', 'Info']
-args2 = [TREESEARCH_EXCTBL, '-s', 'stepwiseaddition', '-a', 'spr', '-l', 'Info']
-args3 = [TREESEARCH_EXCTBL, '-s', 'stepwiseaddition', '-a', 'combo', '-l', 'Info']
-args4 = [TREESEARCH_EXCTBL, '-s', 'random', '-a', 'nni', '-l', 'Info']
-args5 = [TREESEARCH_EXCTBL, '-s', 'random', '-a', 'spr', '-l', 'Info']
-args6 = [TREESEARCH_EXCTBL, '-s', 'random', '-a', 'combo', '-l', 'Info']
+args1 = [TREESEARCH_EXCTBL, '-a', 'nni', '-l', 'Info']
+args2 = [TREESEARCH_EXCTBL, '-a', 'spr', '-l', 'Info']
+args3 = [TREESEARCH_EXCTBL, '-a', 'combo', '-l', 'Info']
 
-configs = [args1, args2, args3, args4, args5, args6]
+configs = [args1, args2, args3]
+
+starttrees = [['-s', 'random'],['-s','stepwiseaddition']]
+
+def parse_lqic(out):
+    outString = out.decode("UTF-8")
+    str_pos = outString.find("INFO Sum lqic final Tree:",0)
+    if str_pos != -1:
+        start = str_pos + 25
+        end = outString.find("\n",str_pos)
+        lqic = float(outString[start:end])
+        return lqic
+
 
 for d in data:
-    for arguments in configs:
-        a = arguments + ['-e', d[0], '-r', d[1]]
-        print("----------------------------------------")
-        print(" ".join(a))
-        start = timeit.default_timer()
-        process = subprocess.Popen(a, stdout = subprocess.PIPE, stderr=subprocess.STDOUT)
+    for s in starttrees:
+        file_st = '../../out/starttree.tre'
+        process = subprocess.Popen([TREESEARCH_EXCTBL, '-a', 'no', '-o', file_st]+s,
+                                   stdout = subprocess.PIPE, stderr=subprocess.STDOUT)
         out, err = process.communicate()
-        end = timeit.default_timer()
-        print( "took " + str(end - start) + "s")
-        outString = out.decode("UTF-8")
-        str_pos = outString.find("INFO Sum lqic final Tree:",0)
-        if str_pos != -1:
-            start = str_pos + 25
-            end = outString.find("\n",str_pos)
-            lqic = float(outString[start:end])
+        lqic = parse_lqic(out)
+        print("sum lqic of startTree: " + str(lqic))
+        for arguments in configs:
+            a = arguments + ['-e', d[0], '-r', d[1], '-t', file_st]
+            print("----------------------------------------")
+            print(" ".join(a))
+            start = timeit.default_timer()
+            process = subprocess.Popen(a, stdout = subprocess.PIPE, stderr=subprocess.STDOUT)
+            out, err = process.communicate()
+            end = timeit.default_timer()
+            print( "took " + str(end - start) + "s")
+            lqic = parse_lqic(out)
             print("sum lqic: " + str(lqic))
-        (rf_plain, rf_normalized) = compare_rf('../../out/out.tre', d[1])
-        print("plain RF distance: " + rf_plain)
-        print("normalized RF distance: " + rf_normalized)
+            (rf_plain, rf_normalized) = compare_rf('../../out/out.tre', d[1])
+            print("plain RF distance: " + rf_plain)
+            print("normalized RF distance: " + rf_normalized)
 
